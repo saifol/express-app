7bef5e85287d5b82b56c5824ccf09b44d3c950b5
[ahmed] <sahmed53@jhu.edu>
2021-02-28 11:49:38 -0500

    added Procfile for Heroku

--- index.js
+++ index.js
@@ -7,84 +7,84 @@ app.use(express.static("assets"));
 nunjucks.configure("views", {
     autoescape: true,
     express: app,
-  });  
-const port = 5000;
+});
+const port = process.env.PORT || 5000;
 
 // sample schools
 schools = [
-  {
-    Name: "School of Medicine",
-  },
-  {
-    Name: "Whiting School of Engineering",
-  },
+    {
+        Name: "School of Medicine",
+    },
+    {
+        Name: "Whiting School of Engineering",
+    },
 ];
 
 // sample terms
 terms = [
-  {
-    Name: "Fall 2020",
-  },
-  {
-    Name: "Summer 2020",
-  },
-  {
-    Name: "Spring 2020",
-  },
+    {
+        Name: "Fall 2020",
+    },
+    {
+        Name: "Summer 2020",
+    },
+    {
+        Name: "Spring 2020",
+    },
 ];
 
 // sample courses
 courses = [
-  {
-    OfferingName: "EN.601.226",
-    SectionName: "01",
-    Title: "Data Structures",
-    Instructors: "A. Madooei",
-  },
-  {
-    OfferingName: "EN.601.226",
-    SectionName: "02",
-    Title: "Data Structures",
-    Instructors: "A. Madooei",
-  },
-  {
-    OfferingName: "EN.601.280",
-    SectionName: "01",
-    Title: "Full-Stack JavaScript",
-    Instructors: "A. Madooei",
-  },
-  {
-    OfferingName: "EN.601.280",
-    SectionName: "02",
-    Title: "Full-Stack JavaScript",
-    Instructors: "A. Madooei",
-  },
+    {
+        OfferingName: "EN.601.226",
+        SectionName: "01",
+        Title: "Data Structures",
+        Instructors: "A. Madooei",
+    },
+    {
+        OfferingName: "EN.601.226",
+        SectionName: "02",
+        Title: "Data Structures",
+        Instructors: "A. Madooei",
+    },
+    {
+        OfferingName: "EN.601.280",
+        SectionName: "01",
+        Title: "Full-Stack JavaScript",
+        Instructors: "A. Madooei",
+    },
+    {
+        OfferingName: "EN.601.280",
+        SectionName: "02",
+        Title: "Full-Stack JavaScript",
+        Instructors: "A. Madooei",
+    },
 ];
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 // index.js file
 app.get("/dashboard", (req, res) => {
     let data = {
-      uname: req.query.uname,
+        uname: req.query.uname,
     };
     res.render("dashboard.njk", data);
-  });
-  
+});
+
 app.get("/api/schools", (req, res) => {
-  res.json(schools);
+    res.json(schools);
 });
 
 app.get("/api/terms", (req, res) => {
-  res.json(terms);
+    res.json(terms);
 });
 
 app.get("/api/courses", (req, res) => {
-  res.json(courses);
+    res.json(courses);
 });
 
 app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
+    console.log(`Express app listening at http://localhost:${port}`);
 });
